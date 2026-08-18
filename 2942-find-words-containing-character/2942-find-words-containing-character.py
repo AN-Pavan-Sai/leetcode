5d870a5de91e3bdb6a61f892823228e413599137
AN-Pavan-Sai <student102006@gmail.com>
2026-08-18 12:19:31 +0530

    Time: 2 ms (43.04%), Space: 12.2 MB (91.94%) - LeetHub

--- 2942-find-words-containing-character/2942-find-words-containing-character.py
+++ 2942-find-words-containing-character/2942-find-words-containing-character.py
@@ -5,9 +5,7 @@ class Solution(object):
         :type x: str
         :rtype: List[int]
         """
-
         lst = []
-
         for i in range(len(words)):
             if x in words[i]:
                 lst.append(i)
